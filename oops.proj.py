95b94e267f2bf7908f5f9e690b2ee3af6b219038
Sandy <elecdeep9754@gmail.com>
2025-09-22 18:51:55 +0530

    signup and signin feature added

--- oops.proj.py
+++ oops.proj.py
@@ -8,16 +8,18 @@ class chatbook:
         self.menu()
     
     def menu(self): 
-        user_input = input("""Welcome to chatbook !! How would you like to proceed? 
-                 1.Press 1 to signup
-                 2.press 2 to signin   
-                 3.press 3 to  write a post
-                 4.press 4 to message a friend
-                 5-press any other key to  exit""")
+        user_input = input(
+            "Welcome to chatbook !! How would you like to proceed?\n"
+            "1.Press 1 to signup\n"
+            "2.press 2 to signin\n"
+            "3.press 3 to write a post\n"
+            "4.press 4 to message a friend\n"
+            "5-press any other key to exit"
+        )
         if user_input == "1":
-            pass 
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -25,4 +27,31 @@ class chatbook:
         else: 
             exit()
             
-obj = chatbook()      
+
+    
+    def signup(self):
+        email = input("enter your email here ->")
+        pwd  = input("setup your password here->")
+        self.username = email 
+        self.password = pwd
+        print("You have signed up successfully !!")
+        print("\n")
+        self.menu()
+
+     
+    def signin(self):
+        if self.username =='' and self.password =='':
+            print("please singnup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here ->")
+            pwd =input("Enter your password here ->")
+            if self.username == uname and self.password == pwd:
+                print("You have signed in successfully !!")
+                self.loggedin =True
+            else:
+                print("Please input correct credentials.. !!") 
+        print("\n")
+        self.menu()           
+        
+obj = chatbook()   
+ 
